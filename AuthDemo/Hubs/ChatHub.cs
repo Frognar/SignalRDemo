f3e94d40ab2b581dbe069c674cf960112f596d2d
frognar <sebastian.przyszlak@gmail.com>
2025-01-24 08:57:24 +0100

    Update ChatHub to get username from a client

--- AuthDemo/Hubs/ChatHub.cs
+++ AuthDemo/Hubs/ChatHub.cs
@@ -6,26 +6,14 @@ namespace AuthDemo.Hubs;
 [Authorize]
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string who, string message)
+    public async Task SendMessage(string toWho, string user, string message)
     {
-        string? name = Context.User?.Identity?.Name;
-        if (string.IsNullOrEmpty(name))
-        {
-            return;
-        }
-
-        await Clients.Groups(who).SendAsync("ReceiveMessage", $"{name}: {message}");
+        await Clients.Groups(toWho).SendAsync("ReceiveMessage", user, message);
     }
 
     public override async Task OnConnectedAsync()
     {
-        string? name = Context.User?.Identity?.Name;
-        if (string.IsNullOrEmpty(name))
-        {
-            await base.OnConnectedAsync();
-            return;
-        }
-        
+        string name = Context.User!.Identity!.Name!;
         await Groups.AddToGroupAsync(Context.ConnectionId, name);
         await base.OnConnectedAsync();
     }
